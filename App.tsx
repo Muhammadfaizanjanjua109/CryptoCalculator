62e738c638484096e3cb5e47cf2cc9e840696f2d
Muhammadfaizanjanjua109 <muhammadfaizanjanjua109@gmail.com>
2022-08-04 16:50:51 +0500

    Basic Working for BNB completed

--- App.tsx
+++ App.tsx
@@ -2,10 +2,82 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [modal, setModal] = React.useState({
+    TotalMoneyInve: 10000,
+    purchasingRateDollar: 205,
+    purchasingRateCyrpto: 269,
+    sellingRateDollar: 220,
+    sellingRateCrypto: 305,
+  });
+  const [profit, setProfit] = React.useState(null);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setModal((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleChangeSubmit = (e) => {
+    e.preventDefault();
+
+    const totalMoneyInvestedDollars =
+      modal.TotalMoneyInve / modal.purchasingRateDollar;
+    console.log(totalMoneyInvestedDollars, 'totalMoneyInvestedDollars');
+    const totalCryptoBuying =
+      totalMoneyInvestedDollars / modal.purchasingRateCyrpto;
+    console.log(totalCryptoBuying, 'totalCryptoBuying');
+    const totalCryptoProfit = totalCryptoBuying * modal.sellingRateCrypto;
+    console.log(totalCryptoProfit, 'totalCryptoProfit');
+    const totalEarning = totalCryptoProfit * modal.sellingRateDollar;
+    console.log(totalEarning, 'totalEarning');
+   
+    setProfit(totalEarning);
+  };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <form>
+        <label>Enter Total Money in RS</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={modal.TotalMoneyInve}
+          name="TotalMoneyInve"
+        />
+        <br />
+        <label>Purchasing Rate Dollar</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={modal.purchasingRateDollar}
+          name="purchasingRateDollar"
+        />
+        <br />
+
+        <label>Purchasing Rate of Crypto</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={modal.purchasingRateCyrpto}
+          name="purchasingRateCyrpto"
+        />
+        <br />
+        <label>Selling Rate of Dollar</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={modal.sellingRateDollar}
+          name="sellingRateDollar"
+        />
+        <br />
+        <label>Selling Rate of Crypto</label>
+        <input
+          type="number"
+          onChange={handleChange}
+          value={modal.sellingRateCrypto}
+          name="sellingRateCrypto"
+        />
+        <button onClick={(e) => handleChangeSubmit(e)}>Submit</button>
+      </form>
+      <p>Your Answer is </p>
+      {profit && profit}
     </div>
   );
 }
